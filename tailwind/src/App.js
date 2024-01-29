4637f6aea5bd56372117f980b052e1cc21920b52
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-01-29 12:27:34 +0530

    Created few components and tried tailwind.css functionality on it

--- tailwind/src/App.js
+++ tailwind/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import Navbar from './Components/Navbar'
+import Main from './Components/Main'
+import Footer from './Components/Footer'
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <Navbar/>
+      <Main/>
+      <Footer/>
+    </div>
+
   )
 }
 
---
+++ tailwind/src/Components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex bg-black text-slate-500 px-10 p-5 mx-auto gap-20 text-xl '>
+      <a href='#' className=''>Home</a>
+      <a href='#'>About</a>
+      <a href='#'>Contact</a>
+      <a href='#'>Address</a>
+      <a href='#'>Testimonials</a>
+    </div>
+  )
+}
+
+export default Navbar
