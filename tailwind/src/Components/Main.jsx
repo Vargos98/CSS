ccfb07e64baf2683b0138d86f2ba7c40ea6f453e
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-01-29 12:56:12 +0530

    Update Main.jsx

--- tailwind/src/Components/Main.jsx
+++ tailwind/src/Components/Main.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Main = () => {
   return (
-    <div>Main</div>
+    <div className=' bg-gradient-to-b from-black to-gray-700 h-screen w-full'>
+      <h1 className='text-slate-500 text-center text-2xl justify-center align-center'>Main</h1>
+    </div>
   )
 }
 
